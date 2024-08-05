90a3f5cef67a5390afa6b93e734acf0160e824cd
Cassese03 <90678655+Cassese03@users.noreply.github.com>
2024-08-05 15:30:00 +0200

    Dimensioni QRCode

--- resources/views/qrcoderesult.blade.php
+++ resources/views/qrcoderesult.blade.php
@@ -2,23 +2,24 @@
 <head>
     <style>
         @page {
-            size: 30mm 70mm;
+            size: 40mm 18mm;
             margin: 0;
         }
     </style>
     <title>QRCode</title>
 </head>
 <body>
-<div style="border-width: 3px;border-style: solid;border-color:black;height: 30mm;width: 70mm;">
-    <div style="display: flex;margin:2%;">
-        <img id='barcode'
-             src="https://api.qrserver.com/v1/create-qr-code/?data={{$alias.';'.$scadenza.';'.$lotto}}&amp;size=100x100"
-             alt="QRCode" title="QRCode" width="100" height="100"/>
-        <div style="display:flex;flex-direction:column;">
-            <div style="padding:8%;padding-left: 50%!important;"><a style="font-weight: bolder;">{{$alias}}</a></div>
-            <div style="padding:8%;padding-left: 50%!important;"><a style="font-weight: bolder;">{{$scadenza}}</a></div>
-            <div style="padding:8%;padding-left: 50%!important;"><a style="font-weight: bolder;">{{$lotto}}</a></div>
-        </div>
+<div style="display: flex;height: 16mm;width: 38mm;margin:0;overflow: hidden;">
+    <img id='barcode'
+         src="https://api.qrserver.com/v1/create-qr-code/?data={{$alias.';'.$scadenza.';'.$lotto}}&amp;size=50x50"
+         alt="QRCode" title="QRCode" width="50" height="50"/>
+    <div style="display:flex;flex-direction:column">
+        <div style="padding:4%;padding-left: 15%!important;"><a
+                style="font-size:10px;font-weight: bolder;">{{$alias}}</a></div>
+        <div style="padding:4%;padding-left: 15%!important;"><a
+                style="font-size:10px;font-weight: bolder;">{{$scadenza}}</a></div>
+        <div style="padding:4%;padding-left: 15%!important;"><a
+                style="font-size:10px;font-weight: bolder;">{{$lotto}}</a></div>
     </div>
 </div>
 </body>
